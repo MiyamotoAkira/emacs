6b022f40fe334c893597bfba7b2d39989228fb79
Jorge Gueorguiev Garcia <jorge.garcia@veinterative.com>
2015-08-14 10:26:15 +0100

    Added emacs ignores to .gitignore. Drastically changed the emacs configuration to add loads of missing niceties, especially for lips/clojure development

--- emacs.el
+++ emacs.el
@@ -18,15 +18,63 @@
 
 (require 'package-x)
 
-
 (package-initialize)
 
+; less refresh the packages
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; we define the packages that we weant to upload
+(defvar my-packages
+  '(
+   paredit
+   clojure-mode
+   clojure-mode-extra-font-locking
+   cider
+   ido-ubiquitous
+   smex
+   rainbow-delimiters
+   tagedit
+   magit
+   elixir-mode
+   go-mode
+   haskell-mode))
+
+; macos special path info (shell and non-shell apps get different paths)
+; not sure if needed due to the below
+;(if (eq system-type 'darwin)
+;	(add-to-list 'my-packages 'exec-path-from-shell))
+
+; we upload the whole lot
+(dolist (pa my-packages)
+  (unless (package-installed-p pa)
+	(package-install pa)))
+
+
+; general modifications
 (require 'ido)
 (ido-mode t)
+(ido-ubiquitous-mode 1)
+(setq ido-auto-merge-work-directories-length -1)
 
-(add-to-list 'load-path "~/Misc/emacs/go-mode.el")
-(require 'go-mode-load)
+(setq smex-save-file (concat user-emacs-directory ".smex-items"))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
 
+(show-paren-mode 1)
+; highlight current line
+;(global-hl-line-mode 1)
+
+; line numbers
+(global-linum-mode)
+
+;; full path in title bar
+(setq-default frame-title-format "%b (%f)")
+
+;; no bell
+(setq ring-bell-function 'ignore)
+
+; Go modifications
 (defun set-exec-path-from-shell-PATH ()
   (let ((path-from-shell (replace-regexp-in-string
 						  "[ \t\n]*$"
@@ -52,4 +100,49 @@
   ; Godef jump key binding
   (local-set-key (kbd "M-.") 'godef-jump))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
-(add-hook 'haskell-mode-hook 'haskell-indentation-mode)  
+
+; Haskell modifications
+(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+
+; Clojure modifications
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+(add-hook 'clojure-mode-hook 'subword-mode)
+(require 'clojure-mode-extra-font-locking)
+(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+
+;; syntax hilighting for midje
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (setq inferior-lisp-program "lein repl")
+            (font-lock-add-keywords
+             nil
+             '(("(\\(facts?\\)"
+                (1 font-lock-keyword-face))
+               ("(\\(background?\\)"
+                (1 font-lock-keyword-face))))
+            (define-clojure-indent (fact 1))
+            (define-clojure-indent (facts 1))))
+
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+
+; lisp modifications
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
+(add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'rainbow-delimiters-mode)
+(add-hook 'ielm-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'lisp-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'lisp-interaction-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'schem-mode-hook #'rainbow-delimiters-mode)
+
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
