28032b51a83974e34666c61a46f6374b34503783
MiyamotoAkira <yefo.akira@gmail.com>
2016-04-06 08:42:05 +0100

    Added keybindings
    I have added keybindings for omnisharp mode, so I can run more easily the
    omnisharp commands. Probably will end adding more

--- languages/init-csharp.el
+++ languages/init-csharp.el
@@ -9,4 +9,17 @@
 
 (add-hook 'csharp-mode-hook 'omnisharp-mode)
 
+(define-key omnisharp-mode-map (kbd "C-c o u") 'omnisharp-fix-usings)
+(define-key omnisharp-mode-map (kbd "C-c o s") 'omnisharp-start-omnisharp-server)
+(define-key omnisharp-mode-map (kbd "C-c o b") 'omnisharp-build-in-emacs)
+(define-key omnisharp-mode-map (kbd "C-c o t a") 'omnisharp-unit-test-all)
+(define-key omnisharp-mode-map (kbd "C-c o t s") 'omnisharp-unit-test-single)
+(define-key omnisharp-mode-map (kbd "C-c o t f") 'omnisharp-unit-test-fixture)
+(define-key omnisharp-mode-map (kbd "C-c o t w") 'omnisharp-unit-test-worker)
+(define-key omnisharp-mode-map (kbd "C-c o r r") 'omnisharp-rename)
+(define-key omnisharp-mode-map (kbd "C-c o r i") 'omnisharp-rename-interactively)
+(define-key omnisharp-mode-map (kbd "C-c o f u") 'omnisharp-find-usages)
+(define-key omnisharp-mode-map (kbd "C-c o f i") 'omnisharp-find-implementations)
+
+
 (provide 'init-csharp)
