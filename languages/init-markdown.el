991a4603aba53e6c91c47b9fd4e5194917910fcd
MiyamotoAkira <yefo.akira@gmail.com>
2017-08-27 17:22:56 +0100

    Added pandoc processor
    This will need to be installed in other computers where I have emacs installed

--- languages/init-markdown.el
+++ languages/init-markdown.el
@@ -5,5 +5,7 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
+(custom-set-variables
+ '(markdown-command "/usr/bin/pandoc"))
 (provide 'init-markdown)
 ;;; init-markdown.el ends here
