bbe4e30405e5d4a9cafa0dbfc04ac44e50bdb3c3
MiyamotoAkira <yefo.akira@gmail.com>
2017-09-04 21:11:41 +0100

    Eliminated underline on presentation
    Very annoying for a presentationt o have the highlighting of the line
    to appear

--- init.el
+++ init.el
@@ -234,12 +234,14 @@
             (lambda ()
               (org-present-big)
               (org-display-inline-images)
-              (global-linum-mode -1)))
+              (global-linum-mode -1)
+              (global-hl-line-mode -1)))
   (add-hook 'org-present-mode-quit-hook
             (lambda ()
               (org-present-small)
               (org-remove-inline-images)
-              (global-linum-mode))))
+              (global-linum-mode)
+              (global-hl-line-mode 1))))
 
 (use-package neotree
   :bind (([f8] . neotree-toggle)))
