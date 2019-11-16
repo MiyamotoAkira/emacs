014fa4117f7a7ea084e7492fcdb7c9b71a5a3c07
MiyamotoAkira <yefo.akira@gmail.com>
2019-11-16 08:54:21 +0000

    Fixing resolution issues
    We tried to use diminish before it was installed

--- init.el
+++ init.el
@@ -26,6 +26,9 @@
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
+(unless (package-installed-p 'diminish)
+  (package-install 'diminish))
+
 (eval-when-compile
   (require 'use-package))
 
