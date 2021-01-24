321abc2e2f6a7cf1397b360c24f7864e30fd1137
MiyamotoAkira <yefo.akira@gmail.com>
2021-01-24 10:48:13 +0000

    Improving the setup of shell variables

--- submodules/init-nix.el
+++ submodules/init-nix.el
@@ -7,9 +7,19 @@
 
 ;;; Code:
 (use-package exec-path-from-shell
-  :ensure t)
-
-(exec-path-from-shell-initialize)
+  :ensure t
+  :config
+  (setq exec-path-from-shell-variables '("PATH"
+                                         "ZSH"
+                                         "PYENV_ROOT"
+                                         "VIRTUALENVWRAPPER_PYTHON"
+                                         "PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV"
+                                         "WORKON_HOME"
+                                         "PROJECT_HOME"
+                                         "ANDROID_HOME"
+                                         "JAVA_HOME"
+                                         "SDKMAN_DIR"))
+  (exec-path-from-shell-initialize))
 
 (provide 'init-nix)
 ;;; init-nix.el ends here
