fe75d5b633cd45452d261cffe618b04e265dc16b
MiyamotoAkira <yefo.akira@twoormore.eu>
2023-09-01 18:15:09 +0100

    Fixed mispelling of command

--- submodules/init-tools.el
+++ submodules/init-tools.el
@@ -214,9 +214,9 @@ The behaviour change if you pass the default UNIVERSAL argument.  Without it, a 
   :defer 2)
 
 (if (file-directory-p "~/code/personal/structurizr-mode")
-    (progrn
-     (add-to-list 'load-path "~/code/personal/structurizr-mode")
-     (require 'structurizr-mode)))
+    (progn
+      (add-to-list 'load-path "~/code/personal/structurizr-mode")
+      (require 'structurizr-mode)))
 
 (use-package plantuml-mode
   :config
