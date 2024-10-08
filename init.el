68f86bbf0284c8b415eaa4dee51d6952b7515ffa
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2024-10-08 14:06:35 +0100

    Starting the process to move configuration into org files
    And adding a new package on the way

--- init.el
+++ init.el
@@ -23,30 +23,34 @@
                               (time-subtract after-init-time before-init-time)))
                      gcs-done)))
 
-  ;; we want custom setup on a separate file. This is set because all the variables
-  ;; that appear there are set on the different .el setup files I have
+;; we want custom setup on a separate file. This is set because all the variables
+;; that appear there are set on the different .el setup files I have
 
-  
+
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
-  ;; We are organizing files in a sub-directory
+;; We are organizing files in a sub-directory
 
 
 (add-to-list 'load-path (expand-file-name "submodules" user-emacs-directory))
 
 (require 'init-packaging)
 
+;; This is to move setup into an org file
+(require 'org)
+(org-babel-load-file (expand-file-name "org-configuration.org" user-emacs-directory))
+
 (require 'bind-key)
 
 (setq org-roam-v2-ack t)
 
-  ;; ;; macos special path info (shell and non-shell apps get different paths)
-  ;; ;; not sure if needed due to the below
-  ;; ;;(if (eq system-type 'darwin)
-  ;; ;;	(add-to-list 'my-packages 'exec-path-from-shell))
+;; ;; macos special path info (shell and non-shell apps get different paths)
+;; ;; not sure if needed due to the below
+;; ;;(if (eq system-type 'darwin)
+;; ;;	(add-to-list 'my-packages 'exec-path-from-shell))
 
-  ;; This is to get the path variable read from
-  ;; the shell environment.
+;; This is to get the path variable read from
+;; the shell environment.
 
 
 (if (memq window-system '(mac ns))
@@ -56,9 +60,9 @@
     (require 'init-nix)
   (require 'init-windows))
 
-  ;; If we start the daemon we are on a nix system
+;; If we start the daemon we are on a nix system
 
-  
+
 (when (daemonp)
   (require 'init-nix))
 
